e3dd63eb67462deb386d9656aa14eb9d35de7061
Padma Pooja Shetty <padmapoojashetty86@gmail.com>
2026-02-06 14:47:34 +0530

    Initial commit - Todo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const addOrUpdateTask = () => {
+    if (task.trim() === "") return;
+
+    if (editIndex !== null) {
+      
+      const updatedTasks = [...tasks];
+      updatedTasks[editIndex] = task;
+      setTasks(updatedTasks);
+      setEditIndex(null);
+    } else {
+      
+      setTasks([...tasks, task]);
+    }
+
+    setTask("");
+  };
+
+ 
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const editTask = (index) => {
+    setTask(tasks[index]);
+    setEditIndex(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>To Do App</h2>
+
+      <div className="input-box">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+        />
+        <button onClick={addOrUpdateTask}>
+          {editIndex !== null ? "Update" : "Add"}
+        </button>
+      </div>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t}
+            <div>
+              <button
+                onClick={() => editTask(index)}
+                style={{ marginRight: "6px" }}
+              >
+                ✏️
+              </button>
+              <button
+                className="delete-btn"
+                onClick={() => deleteTask(index)}
+              >
+                ❌
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
